d47252416d845caf1b76bc3bcfb717cf17b0428a
Sagar2177 <sagar.sharma2177@gmail.com>
2022-05-18 12:21:46 +0530

    deployment.sh added

--- deployment.sh
+++ deployment.sh
@@ -2,7 +2,7 @@
 echo ‘installing requirements…’ 
 npm install
 echo ‘start server…’
-forever start -c “npm start” ./
+forever start -c “npm start” ./test-app/
 node test.js ./test-app/
 echo ‘started server. ending SSH session..’
 exit
