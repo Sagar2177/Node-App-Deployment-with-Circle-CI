eb66be9eab2b5201e2dccb27ac783093b3d8ae19
Sagar2177 <sagar.sharma2177@gmail.com>
2022-05-18 13:00:24 +0530

    yml updated

--- deployment.sh
+++ deployment.sh
@@ -3,6 +3,6 @@ echo ‘installing requirements…’
 apt-get install -y npm
 npm install
 echo ‘start server…’
-forever start -c “npm start” ./
+npm start ./
 echo ‘started server. ending SSH session..’
 exit
